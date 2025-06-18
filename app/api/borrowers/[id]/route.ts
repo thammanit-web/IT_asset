69fcff668cced2007941e313159f4e48168d0d32
thammanit-web <thammanitrinthang@example.com>
2025-06-18 15:40:55 +0700

    fix 101

--- app/api/borrowers/[id]/route.ts
+++ app/api/borrowers/[id]/route.ts
@@ -2,8 +2,8 @@ import { NextResponse,NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
 // GET /api/borrowers/[id] - Get borrower by ID
-export async function GET(request: NextRequest, context: { params: { id: string } }) {
-  const borrowerId = parseInt(context.params.id, 10);
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const borrowerId = parseInt(params.id, 10);
 
   if (isNaN(borrowerId)) {
     return NextResponse.json({ error: 'Invalid Borrower ID' }, { status: 400 });
@@ -25,8 +25,8 @@ export async function GET(request: NextRequest, context: { params: { id: string 
 }
 
 // PUT /api/borrowers/[id] - Update borrower by ID
-export async function PUT(request: NextRequest, context: { params: { id: string } }) {
-  const borrowerId = parseInt(context.params.id, 10);
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const borrowerId = parseInt(params.id, 10);
 
   if (isNaN(borrowerId)) {
     return NextResponse.json({ error: 'Invalid Borrower ID' }, { status: 400 });
@@ -34,7 +34,7 @@ export async function PUT(request: NextRequest, context: { params: { id: string 
 
   let data: any;
   try {
-    data = await request.json();
+    data = await req.json();
     const updatedBorrower = await prisma.borrower.update({
       where: { id: borrowerId },
       data: data,
@@ -53,8 +53,8 @@ export async function PUT(request: NextRequest, context: { params: { id: string 
 }
 
 // DELETE /api/borrowers/[id] - Delete borrower by ID
-export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
-  const borrowerId = parseInt(context.params.id, 10);
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+  const borrowerId = parseInt(params.id, 10);
   if (isNaN(borrowerId)) {
     return NextResponse.json({ error: 'Invalid Borrower ID' }, { status: 400 });
   }
